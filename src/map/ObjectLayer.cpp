21129fde8947172b87551b72e6301f3d4c9e326e
Csaba Ekart <csaba.ekart@gmail.com>
2023-03-31 09:24:56 +0200

    iterate through properties

--- src/map/ObjectLayer.cpp
+++ src/map/ObjectLayer.cpp
@@ -5,7 +5,14 @@ namespace bgl
 {
 	ObjectLayer::ObjectLayer(tmx::ObjectGroup& objectGroup) : m_ObjectGroup(objectGroup)
 	{
-
+		const auto& properties = objectGroup.getProperties();
+		for (const auto& prop : properties)
+		{
+			if (prop.getName() == "solid" && prop.getType() == tmx::Property::Type::Boolean && prop.getBoolValue())
+			{
+				
+			}
+		}
 	}
 
 	ObjectLayer::~ObjectLayer()
