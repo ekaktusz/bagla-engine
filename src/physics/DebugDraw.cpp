ccdf9696d3b4320f6c66b3999b8dd9708ec35f8f
Csaba Ekart <csaba.ekart@gmail.com>
2023-04-15 22:32:00 +0200

    debug draw base

---
+++ src/physics/DebugDraw.cpp
@@ -0,0 +1,42 @@
+#include "physics/DebugDraw.h"
+#include <spdlog/spdlog.h>	
+
+namespace bgl
+{
+
+	void DebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+	void DebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+	void DebugDraw::DrawCircle(const b2Vec2& center, float radius, const b2Color& color)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+	void DebugDraw::DrawSolidCircle(const b2Vec2& center, float radius, const b2Vec2& axis, const b2Color& color)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+	void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+	void DebugDraw::DrawTransform(const b2Transform& xf)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+	void DebugDraw::DrawPoint(const b2Vec2& p, float size, const b2Color& color)
+	{
+		spdlog::warn("The method or operation is not implemented.");
+	}
+
+}
