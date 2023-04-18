96b9057aa09e033a468292a383ada97c4a3a8c29
Csaba Ekart <csaba.ekart@gmail.com>
2023-04-18 12:16:54 +0200

    update rigidbody to scaling

--- src/physics/RigidBody.cpp
+++ src/physics/RigidBody.cpp
@@ -18,10 +18,10 @@ namespace bgl
 
 		if (dynamic) m_BodyDef.type = b2_dynamicBody;
 
-		float bx = (x + sx / 2) / PPM;
-		float by = -(y + sy / 2) / PPM;
+		float bx = PhysicsWorld::scaleToPhysics(x + sx / 2) ;
+		float by = -PhysicsWorld::scaleToPhysics(y + sy / 2);
 		m_BodyDef.position.Set(bx, by);
-		m_Shape.SetAsBox(sx / PPM / 2, sy / PPM / 2);
+		m_Shape.SetAsBox(PhysicsWorld::scaleToPhysics(sx) / 2, PhysicsWorld::scaleToPhysics(sy) / 2);
 
 		m_Body = PhysicsWorld::getInstance().m_World->CreateBody(&m_BodyDef);
 		//m_Body = world.CreateBody(&m_BodyDef);
@@ -34,7 +34,6 @@ namespace bgl
 
 		m_Fixture = m_Body->CreateFixture(&fixtureDef);
 		m_Body->SetFixedRotation(true);
-		
 	}
 
 	RigidBody::RigidBody(sf::Vector2f position, sf::Vector2f size, bool dynamic /*= true*/, float density) : RigidBody(position.x, position.y, size.x, size.y, dynamic, density)
@@ -51,14 +50,14 @@ namespace bgl
 
 	sf::Vector2f RigidBody::getPosition() const
 	{
-		float x = m_Body->GetPosition().x * PPM;
-		float y = -m_Body->GetPosition().y * PPM;
+		float x = PhysicsWorld::scaleToGraphics(m_Body->GetPosition().x);
+		float y = -PhysicsWorld::scaleToGraphics(m_Body->GetPosition().y);
 		return sf::Vector2f(x - m_RigidBodyRectangleShape.getSize().x / 2, y - m_RigidBodyRectangleShape.getSize().y / 2);
 	}
 
 	void RigidBody::setLinearVelocity(sf::Vector2f velocity)
 	{
-		m_Body->SetLinearVelocity({ velocity.x, velocity.y });
+		m_Body->SetLinearVelocity(PhysicsWorld::scaleToPhysics({ velocity.x, velocity.y }));
 	}
 
 	void RigidBody::setGravityScale(float gravityScale)
