7ad541cd3a79425e511bde70abdea17c29bbf3f8
Csaba Ekart <csaba.ekart@gmail.com>
2023-04-23 20:13:59 +0200

    camera interface

--- include/bagla-engine/camera/Camera.h
+++ include/bagla-engine/camera/Camera.h
@@ -1,12 +1,39 @@
 #pramga once
 
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/View.hpp>
+
+namespace sf
+{
+	class View;
+	class Time;
+}
+
 namespace bgl
 {
 	class Camera
 	{
 	public:
+		Camera(sf::RenderWindow& renderWindow);
+		~Camera();
 
+		void setWorldBoundaries(float x, float y, float w, float h);
+		void setWorldBoundaries(sf::FloatRect boundaries);
+		void setPosition(float x, float y);
+		void setPosition(sf::Vector2f position);
+		sf::Vector2f getPosition() const;
+		void setScale(float scale);
 
+		void setAngle(float angle);
+		float getAngle() const;
+
+		void update(const sf::Time& dt);
+		
 	private:
+		sf::View m_View;
+		sf::RenderWindow& m_AttachedRenderWindow;
+		sf::FloatRect m_WorldBoundaries;
 	};
+
 }
