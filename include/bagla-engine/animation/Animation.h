88e69190c304aa9c455048ebef62a277da0bd470
Csaba Ekart <csaba.ekart@gmail.com>
2023-04-25 10:22:12 +0200

    animation base

---
+++ include/bagla-engine/animation/Animation.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace bgl
+{
+	class Animation
+	{
+	public:
+		Animation(const sf::Texture& texture, sf::Vector2u frameSize, sf::Vector2u startFrameCoordinates, sf::Vector2u endFrameCoordinates, bool repeating = true);
+		~Animation();
+
+		void play();
+		void pause();
+		
+		void setRepeating(bool repeating);
+		bool getRepeating() const;
+
+	private:
+		sf::Texture m_SpriteSheet;
+		sf::Sprite m_Sprite;
+		sf::Clock m_Timer;
+		float m_TimePeriod;
+		sf::IntRect m_CurrentFrame;
+		bool m_Paused;
+		bool repeating;
+	};
+}
