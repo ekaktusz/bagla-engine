88fe809074c23c19031f50bff5b7a48f41815803
Csaba Ekart <csaba.ekart@gmail.com>
2023-05-17 21:18:21 +0200

    animation component changes

--- include/bagla-engine/animation/AnimationComponent.h
+++ include/bagla-engine/animation/AnimationComponent.h
@@ -3,22 +3,32 @@
 #include <string>
 #include <memory>
 #include <unordered_map>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/Drawable.hpp>
 
-namespace sf { class Time; }
+namespace sf 
+{ 
+	class Time; 
+	class RenderTarget;
+}
 
 namespace bgl
 { 
 	class Animation;
 
-	class AnimationComponent
+	class AnimationComponent : public sf::Drawable
 	{
 	public:
 		void addAnimation(const std::string& id, std::unique_ptr<Animation> animation);
 		void removeAnimation(const std::string& id);
 
 		void update(const sf::Time& dt);
+		void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 		void setCurrentAnimation(const std::string& id);
+
+		void setPosition(float x, float y);
+		void setPosition(sf::Vector2f position);
 
 		void pause();
 		void play();
--- src/animation/AnimationComponent.cpp
+++ src/animation/AnimationComponent.cpp
@@ -1,31 +1,59 @@
 #include "animation/AnimationComponent.h"
+#include "animation/Animation.h"
+#include <spdlog/spdlog.h>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
 
 namespace bgl
 {
-
 	void AnimationComponent::addAnimation(const std::string& id, std::unique_ptr<Animation> animation)
 	{
-
+		if (m_Animations.empty()) m_CurrentAnimationID = id;
+		m_Animations.try_emplace(id, std::move(animation));
 	}
 
 	void AnimationComponent::update(const sf::Time& dt)
 	{
+		m_Animations[m_CurrentAnimationID]->update(dt);
+	}
 
+	void AnimationComponent::draw(sf::RenderTarget& target, sf::RenderStates states) const
+	{
+		auto currentAnimationIter = m_Animations.find(m_CurrentAnimationID);
+		target.draw(*currentAnimationIter->second);
 	}
 
 	void AnimationComponent::setCurrentAnimation(const std::string& id)
+	{
+		if (m_CurrentAnimationID == id) return;
+
+		auto found = m_Animations.find(id);
+		if (found == m_Animations.end())
+		{
+			spdlog::error("Can't find animation by id: " + id);
+		}
+		m_Animations[m_CurrentAnimationID]->pause();
+		m_CurrentAnimationID = id;
+		m_Animations[m_CurrentAnimationID]->play();
+	}
+
+	void AnimationComponent::setPosition(float x, float y)
+	{
+		
+	}
+
+	void AnimationComponent::setPosition(sf::Vector2f position)
 	{
 
 	}
 
 	void AnimationComponent::pause()
 	{
-
+		m_Animations[m_CurrentAnimationID]->pause();
 	}
 
 	void AnimationComponent::play()
 	{
-
+		m_Animations[m_CurrentAnimationID]->play();
 	}
-
 }
