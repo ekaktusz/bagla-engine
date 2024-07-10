93a12a78adbf6cba361e1407a402b39d6fee0544
Csaba Ekart <csaba.ekart@gmail.com>
2024-07-10 16:57:20 +0200

    rounded rectangle corrected

--- include/bagla-engine/graphics/RoundedRectangleShape.h
+++ include/bagla-engine/graphics/RoundedRectangleShape.h
@@ -2,76 +2,23 @@
 
 #include <SFML/Graphics/Shape.hpp>
 #include <cmath>
+#include <SFML/System/Vector2.hpp>
 
 namespace bgl
 {
 	class RoundedRectangleShape : public sf::Shape
 	{
 	public:
-		RoundedRectangleShape(const sf::Vector2f& size = sf::Vector2f(0, 0), float radius = 0, unsigned int cornerPoints = 30)
-			: m_size(size), m_radius(radius), m_cornerPoints(cornerPoints)
-		{
-			update();
-		}
+		RoundedRectangleShape(sf::Vector2f size = { 0, 0}, float radius = 0, unsigned int cornerPoints = 30);
 
-		void setSize(const sf::Vector2f& size)
-		{
-			m_size = size;
-			update();
-		}
+		void setSize(sf::Vector2f size);
+		sf::Vector2f getSize() const;
 
-		const sf::Vector2f& getSize() const
-		{
-			return m_size;
-		}
+		void setCornerRadius(float radius);
+		float getCornerRadius() const;
 
-		void setCornerRadius(float radius)
-		{
-			m_radius = radius;
-			update();
-		}
-
-		float getCornerRadius() const
-		{
-			return m_radius;
-		}
-
-		void setCornerPointCount(unsigned int count)
-		{
-			m_cornerPoints = count;
-			update();
-		}
-
-		virtual std::size_t getPointCount() const
-		{
-			return m_cornerPoints * 4;
-		}
-
-		virtual sf::Vector2f getPoint(std::size_t index) const
-		{
-			if (index >= getPointCount())
-				return sf::Vector2f(0, 0);
-
-			float deltaAngle = 90.0f / (m_cornerPoints - 1);
-			sf::Vector2f center;
-			unsigned int centerIndex = index / m_cornerPoints;
-			unsigned int localIndex = index % m_cornerPoints;
-
-			switch (centerIndex)
-			{
-			case 0: center.x = m_size.x - m_radius; center.y = m_radius; break;
-			case 1: center.x = m_radius; center.y = m_radius; break;
-			case 2: center.x = m_radius; center.y = m_size.y - m_radius; break;
-			case 3: center.x = m_size.x - m_radius; center.y = m_size.y - m_radius; break;
-			}
-
-			float angle = deltaAngle * localIndex + 90.0f * centerIndex;
-			float radian = angle * 3.14159265358979323846 / 180.0f;
-
-			return sf::Vector2f(m_radius * std::cos(radian) + center.x,
-				-m_radius * std::sin(radian) + center.y);
-
-		}
+		virtual std::size_t getPointCount() const override;
+		virtual sf::Vector2f getPoint(std::size_t index) const override;
 
 	private:
 		sf::Vector2f m_size;
--- src/graphics/RoundedRectangleShape.cpp
+++ src/graphics/RoundedRectangleShape.cpp
@@ -1,33 +1,64 @@
-////////////////////////////////////////////////////////////
-//
-// By: https://github.com/Overdrivr
-// 
-// This software is provided 'as-is', without any express or implied warranty.
-// In no event will the authors be held liable for any damages arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it freely,
-// subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented;
-// you must not claim that you wrote the original software.
-// If you use this software in a product, an acknowledgment
-// in the product documentation would be appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such,
-// and must not be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source distribution.
-//
-////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
 #include "graphics/RoundedRectangleShape.h"
 #include <cmath>
 
 namespace bgl
 {
+	RoundedRectangleShape::RoundedRectangleShape(sf::Vector2f size, float radius, unsigned int cornerPoints)
+		: m_size(size), m_radius(radius), m_cornerPoints(cornerPoints) 
+	{
+		update();
+	}
 	
+	void RoundedRectangleShape::setSize(sf::Vector2f size)
+	{
+		m_size = size;
+		update();
+	}
+
+	sf::Vector2f RoundedRectangleShape::getSize() const
+	{
+		return m_size;
+	}
+
+	void RoundedRectangleShape::setCornerRadius(float radius)
+	{
+		m_radius = radius;
+		update();
+	}
+
+	float RoundedRectangleShape::getCornerRadius() const
+	{
+		return m_radius;
+	}
+
+	std::size_t RoundedRectangleShape::getPointCount() const
+	{
+		return m_cornerPoints * 4;
+	}
+
+	sf::Vector2f RoundedRectangleShape::getPoint(std::size_t index) const
+	{
+		if (index >= getPointCount())
+			return sf::Vector2f(0, 0);
+
+		float deltaAngle = 90.0f / (m_cornerPoints - 1);
+		sf::Vector2f center;
+		unsigned int centerIndex = index / m_cornerPoints;
+		unsigned int localIndex = index % m_cornerPoints;
+
+		switch (centerIndex)
+		{
+		case 0: center.x = m_size.x - m_radius; center.y = m_radius; break;
+		case 1: center.x = m_radius; center.y = m_radius; break;
+		case 2: center.x = m_radius; center.y = m_size.y - m_radius; break;
+		case 3: center.x = m_size.x - m_radius; center.y = m_size.y - m_radius; break;
+		}
+
+		float angle = deltaAngle * localIndex + 90.0f * centerIndex;
+		float radian = angle * 3.14159265358979323846 / 180.0f;
+
+		return sf::Vector2f(m_radius * std::cos(radian) + center.x,
+			-m_radius * std::sin(radian) + center.y);
+	}
+
 } // namespace sf
