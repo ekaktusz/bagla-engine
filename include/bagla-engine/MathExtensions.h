193e9d9649587a93ff01556eb0b73606332c228e
Csaba Ekart <csaba.ekart@gmail.com>
2024-07-13 14:59:31 +0200

    Update MathExtensions.h

--- include/bagla-engine/MathExtensions.h
+++ include/bagla-engine/MathExtensions.h
@@ -16,4 +16,9 @@ namespace bgl
 		else
 			return c;
 	}
+
+	template <typename T>
+	static T mapValue(T x, T in_min, T in_max, T out_min, T out_max) {
+    return out_min + ((x - in_min) * (out_max - out_min)) / (in_max - in_min);
+	}
 }
