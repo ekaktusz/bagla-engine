fd7918d929ac7d15e9202a1c244ad5ab903362e0
Csaba Ekart <csaba.ekart@gmail.com>
2024-09-23 21:15:55 +0200

    sometinhg

--- include/bagla-engine/IEventHandler.h
+++ include/bagla-engine/IEventHandler.h
@@ -1,10 +1,15 @@
 #pragma once
 
+namespace sf
+{
+class Event;
+}
+
 namespace bgl
 {
-	class IEventHandler
-	{
-
-		virtual ~IEventHandler;
-	};
+class IEventHandler
+{
+	virtual ~IEventHandler = default;
+	virtual void handleEvent(const sf::Event& event) = 0;
+};
 }
--- include/bagla-engine/IGameObject.h
+++ include/bagla-engine/IGameObject.h
@@ -15,6 +15,5 @@ struct GameObject : public sf::Drawable
 
 	virtual void update(const sf::Time& dt) = 0;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
-	virtual void handleEvent(const sf::Event& event) = 0;
 };
 }
